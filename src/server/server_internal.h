2801077f0a75aceb991b26cb0b206cef18966c07
Therm <211634794+Thermirino@users.noreply.github.com>
2026-06-05 19:24:24 +0500

    Add server_internal.h

---
+++ src/server/server_internal.h
@@ -0,0 +1,36 @@
+#ifndef SNAKE_SERVER_INTERNAL
+#define SNAKE_SERVER_INTERNAL
+
+#include <stddef.h>
+#include <poll.h>
+#include <game.h>
+#include <protocol.h>
+
+#define MAX_PENDING_CONNECTIONS     16
+#define MAX_CLIENTS                 16
+
+typedef enum {
+    CLIENT_CONNECTING = 0,
+    CLIENT_CONNECTED
+} client_status;
+
+typedef struct {
+    client_status status;
+    int fd;
+    uint32_t snake_id;
+} client;
+
+typedef struct {
+    int listenfd;
+
+    client clients[MAX_CLIENTS];
+    size_t nclients;
+
+    game_state gs;
+} server_state;
+
+bool server_state_init(server_state* state, const char* port,
+                       int width, int height);
+void server_state_destroy(server_state* state);
+
+#endif
