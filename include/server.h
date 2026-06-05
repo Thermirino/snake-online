cae225a4321bd1f2844a5a5466ed9a085d5b1fbd
Therm <211634794+Thermirino@users.noreply.github.com>
2026-06-05 19:32:20 +0500

    Add main function for server

---
+++ include/server.h
@@ -0,0 +1,8 @@
+#ifndef SNAKE_SERVER_H
+#define SNAKE_SERVER_H
+
+#include <stdbool.h>
+
+bool server_run(const char* port, int width, int height);
+
+#endif
---
+++ src/server/main.c
@@ -0,0 +1,17 @@
+#include "server.h"
+#include <stdio.h>
+#include <server.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>", argv[0]);
+        return -1;
+    }
+
+    int width = 20;
+    int height = 10;
+    if (!server_run(argv[1], width, height))
+        return -1;
+    return 0;
+}
